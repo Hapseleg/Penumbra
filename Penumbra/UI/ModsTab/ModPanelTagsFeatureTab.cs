25c2ca52203b1e159f9092fc9480f99b7756c563
Hapse <hapse@hh4.dk>
2023-06-11 16:19:41 +0200

    Update ModPanelTagsFeatureTab.cs
    added comments for clarity

--- Penumbra/UI/ModsTab/ModPanelTagsFeatureTab.cs
+++ Penumbra/UI/ModsTab/ModPanelTagsFeatureTab.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Dalamud.Interface;
 using ImGuiNET;
 using OtterGui.Raii;
 using OtterGui;
@@ -9,20 +8,22 @@ using System.Numerics;
 using System.Collections.Generic;
 using Dalamud.Logging;
 using System.Linq;
-using Penumbra.Collections;
 using Penumbra.UI.Classes;
 
 namespace Penumbra.UI.ModsTab;
 
 public class ModPanelTagsFeatureTab : ITab
 {
+    
     private readonly ModFileSystemSelector _selector;
     private readonly TutorialService _tutorial;
     private readonly ModManager _modManager;
     private readonly TagButtons _localTags = new();
+
+    //Contains the tags
     private List<List<string>> _tagsList = new();
     //private Dictionary<string, bool> _tagDict = new();
-    private List<string> _selectedTags = new();
+    //private List<string> _selectedTags = new();
 
     public ModPanelTagsFeatureTab(ModFileSystemSelector selector, TutorialService tutorial, ModManager modManager)
     {
@@ -38,7 +39,7 @@ public class ModPanelTagsFeatureTab : ITab
 
     public void DrawContent()
     {
-
+        //Maybe remove this from the description tab?
         var tagIdx = _localTags.Draw("Local Tags: ",
             "Custom tags you can set personally that will not be exported to the mod data but only set for you.\n"
           + "If the mod already contains a local tag in its own tags, the local tag will be ignored.", _selector.Selected!.LocalTags,
@@ -48,6 +49,7 @@ public class ModPanelTagsFeatureTab : ITab
             _modManager.DataEditor.ChangeLocalTag(_selector.Selected!, tagIdx, editedTag);
 
 
+        //Adds the buttons, right now the tags are just in a List that contains List<string>, I'd properly move the tags to a json file so its easier to add and remove tags, can also add a function so the users can add tags themselves
         DrawButtons(_tagsList[0], "Type");
         DrawButtons(_tagsList[1], "Genre");
         DrawButtons(_tagsList[2], "Gear Types");
@@ -55,25 +57,8 @@ public class ModPanelTagsFeatureTab : ITab
         DrawButtons(_tagsList[4], "Gear Styles");
         DrawButtons(_tagsList[5], "Gear Material");
 
-        //ImGui.Separator();
-        //if (ImGui.Button("Save tags"))
-        //{
-        //    for (var i = 0; i < _selectedTags.Count; i++)
-        //    {
-        //        _modManager.DataEditor.ChangeLocalTag(_selector.Selected!, i, _selectedTags[i]);
-        //    }
-        //    _selectedTags.Clear();
-        //}
-        //ImGui.SameLine();
-        //if (ImGui.Button("Print List"))
-        //    foreach (var s in _selectedTags)
-        //        PluginLog.Debug(s);
-
-        //int current = 0;
-        //ImGui.ListBox("Selected Tags", ref current, _selectedTags.ToArray(), _selectedTags.Count);
-
-
     }
+
 
     private void DrawButtons(List<string> tags, string title)
     {
@@ -81,17 +66,20 @@ public class ModPanelTagsFeatureTab : ITab
         //byte buttonsOnSameLine = 0;
         foreach (var tag in tags)
         {
+            //Checks if the mod does not have the tag
             if (!_selector.Selected!.LocalTags.Contains(tag))
             {
-
+                //Clicking the button adds the tag to the localTags list
                 if (ImGui.Button(tag))
                 {
                     AddTag(tag, _selector.Selected!.LocalTags.Count);
                 }
             }
+            //if it does we want to color the button so you can see its added, im pretty sure im using this wrong...?
             else
             {
                 var color = ImRaii.PushColor(ImGuiCol.Button, ColorId.SelectedCollection.Value());
+                //Clicking the button removes the tag from the localTags list
                 if (ImGui.Button(tag))
                 {
                     RemoveTag(_selector.Selected!.LocalTags.IndexOf(tag));
@@ -111,15 +99,13 @@ public class ModPanelTagsFeatureTab : ITab
         ImGui.Separator();
     }
 
-    private void ColorButton()
-    {
-
-    }
-
+    //Adds tag to the mods json
     private void AddTag(string tag, int tagIdx)
     {
         _modManager.DataEditor.ChangeLocalTag(_selector.Selected!, tagIdx, tag);
     }
+
+    //Removes the tag by passing an empty string
     private void RemoveTag(int tagIdx)
     {
         _modManager.DataEditor.ChangeLocalTag(_selector.Selected!, tagIdx, "");
@@ -131,6 +117,7 @@ public class ModPanelTagsFeatureTab : ITab
         return ImGui.IsItemClicked();
     }
 
+    //This is just a hacky test
     private void AddTagsToList()
     {
         List<string> tagTypes = new List<string>
